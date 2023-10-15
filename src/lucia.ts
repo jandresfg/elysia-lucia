476bf5bffcf8bb3ff958e13acf88bf14d2cd1f1b
Andres Fernandez <jandresfgz@gmail.com>
2023-10-15 14:43:43 -0500

    added @lucia-auth/adapter-prisma and set prisma adapter using generated prisma client

--- src/lucia.ts
+++ src/lucia.ts
@@ -1,10 +1,14 @@
+import { prisma } from "@lucia-auth/adapter-prisma";
+import { PrismaClient } from "@prisma/client";
 import { lucia } from "lucia";
 import { elysia } from "lucia/middleware";
 
-// expect error (see next section)
+const prismaCLient = new PrismaClient();
+
 export const auth = lucia({
-  env: "DEV", // "PROD" if deployed to HTTPS
+  env: "DEV", // TODO "PROD" if deployed to HTTPS
   middleware: elysia(),
+  adapter: prisma(prismaCLient),
 });
 
 export type Auth = typeof auth;
